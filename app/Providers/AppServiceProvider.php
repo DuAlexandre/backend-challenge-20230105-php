3a9d43d37ff328aa992e6eb481cbc47a131224b2
Eduardo Alexandre <95940707+DuAlexandre@users.noreply.github.com>
2023-02-19 13:31:32 -0300

    Adding API Status Controller

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,10 @@
 
 namespace App\Providers;
 
+use App\Models\APIStatus;
 use App\Models\Product;
 use App\Repositories\ProductRepositoryEloquent;
+use App\Repositories\APIStatusRepositoryEloquent;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -17,6 +19,12 @@ class AppServiceProvider extends ServiceProvider
 
         $this->app->bind(abstract: 'App\Repositories\ProductRepositoryInterface', concrete: function(){
             return new ProductRepositoryEloquent(new Product());
+        });
+
+        $this->app->bind(abstract: 'App\Repositories\APIStatusRepositoryInterface', concrete: 'App\Repositories\APIStatusRepositoryEloquent');
+
+        $this->app->bind(abstract: 'App\Repositories\APIStatusRepositoryInterface', concrete: function(){
+            return new APIStatusRepositoryEloquent(new APIStatus());
         });
     }
 
