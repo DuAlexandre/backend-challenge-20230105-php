d3af1ddbf886ffb8ce08e7b59358416bc7f2b882
Eduardo Alexandre <95940707+DuAlexandre@users.noreply.github.com>
2023-02-18 21:24:01 -0300

    Adding repositories

---
+++ app/Repositories/ProductRepositoryInterface.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Repositories;
+
+use Illuminate\Database\Eloquent\Model;
+
+interface ProductRepositoryInterface
+{
+    public function __construct(Model $model);
+    public function index();
+    public function show($id);
+    public function store(array $data);
+    public function update($id, array $data);
+    public function destroy($id);
+}
